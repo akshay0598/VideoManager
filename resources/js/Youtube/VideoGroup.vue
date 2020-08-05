6ae561afd0507e34d5cf64d781602708f71f2cdc
iamakshay <sarawgi.akshay05@gmail.com>
2020-08-05 18:25:58 +0530

    Init Commit

---
+++ resources/js/Youtube/VideoGroup.vue
@@ -0,0 +1,58 @@
+<template>
+<div class = "VideoGroup__wrapper row grid">
+<video-item
+v-images-loaded:on.progress="imageProgress"
+v-for="video in videos"
+:video="video"
+:key="video.id.videoId">
+</video-item>
+</div>
+</template>
+<script>
+import Isotope from 'isotope-layout';
+import VideoItem from './VideoItem.vue';
+import imagesLoaded from 'vue-images-loaded';
+export default{
+props:['videos'],
+components:{
+VideoItem
+},
+directives:{
+imagesLoaded
+},
+created(){
+this.relayoutTheGrid();
+},
+data(){
+return{
+isotope:null,
+counter:0
+}
+},
+methods:{
+relayoutTheGrid(){
+var elem = document.querySelector('.grid');
+this.isotope = new Isotope(elem,{
+itemSelector:'.card',
+layoutMode:'masonary'
+});
+},
+imageProgress(instance,img){
+console.log('instance',instance,'img',img);
+this.counter++;
+if(this.counter==20){
+}
+}
+}
+}
+
+</script>
+
+<style lang = "scss">
+.card{
+margin: 0px 5px 5px 0px;
+.card-img-top{
+max-height:210px;
+}
+}
+</style>
---
+++ resources/js/Youtube/YoutubeDash.vue
@@ -0,0 +1,30 @@
+<template>
+<div class = "YoutubeDash__wrapper">
+<video-group :videos = "videos"></video-group>
+</div>
+</template>
+
+<script>
+import Search from './Search';
+import VideoGroup from './VideoGroup.vue';
+export default{
+components:{
+VideoGroup
+},
+created(){
+Search({
+apiKey:'AIzaSyAqEpwVsdiU7njsexjQ-aRM7nds-jz6oQ0',
+term:'laravel repository',
+
+},response => this.videos = response);
+},
+data(){
+return{
+
+videos:null
+}
+}
+
+}
+
+</script>
